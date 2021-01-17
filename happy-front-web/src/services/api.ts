965d4560fcfcfc95a7d0085579006d6cf2456b5f
Gustavo <gustavonikov@gmail.com>
2021-01-17 09:28:16 -0300

    Updating api to heroku

--- happy-front-web/src/services/api.ts
+++ happy-front-web/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // If you are using this app for development, change to http://localhost:8080
-    baseURL: 'happy-backend-nikov.herokuapp.com/',
+    baseURL: 'happy-backend-nikov.herokuapp.com',
 })
 
 export default api;
