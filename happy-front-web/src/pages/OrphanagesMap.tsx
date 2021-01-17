69ed3d47a5ff3be5c77ea05187ab27a344346d2d
Gustavo Chaves <68268899+gustavonikov@users.noreply.github.com>
2021-01-17 03:46:58 -0300

    Update OrphanagesMap.tsx

--- happy-front-web/src/pages/OrphanagesMap.tsx
+++ happy-front-web/src/pages/OrphanagesMap.tsx
@@ -57,7 +57,7 @@ function OrphanagesMap() {
                 zoom={13}
             >
                 {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />  TileLayer do leaflet*/}
-                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`} />
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${REACT_APP_MAPBOX_ACCESS_TOKEN}`} />
                 
                 {orphanages.map((orphanage) => {
                     return (
