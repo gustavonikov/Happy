3d14cc9766905cbfdf71751b6be7b136b504323f
Gustavo Chaves <68268899+gustavonikov@users.noreply.github.com>
2021-01-17 06:39:37 -0300

    Update api.ts

--- happy-front-web/src/services/api.ts
+++ happy-front-web/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // If you are using this app for development, change to http://localhost:8080
-    baseURL: 'https://vercel.com/gustavonikov/proffy-v2-backend/dtsoddthy',
+    baseURL: 'https://proffy-v2-backend-git-master.gustavonikov.vercel.app',
 })
 
 export default api;
